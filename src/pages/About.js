b0b3207aa57b9eb0ee7d8d669c7442663c98741e
Julia von Goetz <jvongoetz808@gmail.com>
2024-05-08 10:40:40 -0700

    router

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Here's how you can reach me...</p>
+    </div>
+  );
+}
+
+export default About;
