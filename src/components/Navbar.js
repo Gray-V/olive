fc046430be66749f29c14eecf6dddddde22a70d2
Julia von Goetz <jvongoetz808@gmail.com>
2024-05-10 14:08:26 -0700

    made hamburger thingy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../logo.png';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-left">
-        <NavLink to="/" className='home-button' > <img className="logo" src={logo} alt="'" /></NavLink>
-        <div className="nav-links">
+        <button className="menu-icon" onClick={toggleMenu}>☰</button>
+        <NavLink to="/" className='home-button' > <img className="logo" src={logo} alt="Logo" /></NavLink>
+        <div className={`nav-links ${isOpen ? "open" : ""}`}>
           <NavLink to="/dashboard" activeClassName="active">Dashboard</NavLink>
           <NavLink to="/technology" activeClassName="active">Technology</NavLink>
           <NavLink to="/pricing" activeClassName="active">Pricing</NavLink>
