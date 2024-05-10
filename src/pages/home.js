c64e2e67170712bd4fc0791fe5689dcf25c09687
Julia von Goetz <jvongoetz808@gmail.com>
2024-05-10 10:07:31 -0700

    landing page base

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import './css/tech.css'; 
+import './css/home.css'; 
+import oliveImage from './olive.png'; 
+import circle from './circle.png';
 
-
-
-function Home() {
+function LandingPage() {
   return (
-    <div className="home-container">
-      <h1>Home Page</h1>
+    <div className="landing-page-container">
+      <header className="landing-header">
+        <div className="logo">Olive</div>
+      </header>
+      <main className="main-content">
+        <h1>Meet Olive</h1>
+        <h2>An AI Calendar Assistant</h2>
+        <p>Olive is an intelligent scheduling app, effortlessly organizing your tasks and appointments while learning your habits for optimal productivity.</p>
+        <button className="try-for-free">Try it for free</button>
+        <img src={oliveImage} alt="Olive" className="olive-image" />
+        <img src={circle} alt="Circle" className="circle" />
+      </main>
     </div>
+
   );
 }
 
-export default Home;
+export default LandingPage;
