687b404c7e02b8b9b2856870a3b834cca2620474
Julia von Goetz <jvongoetz808@gmail.com>
2024-05-10 18:04:54 -0700

    rm thing

--- src/pages/firebase-config.js
+++ src/pages/firebase-config.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
-  };
-  
+    apiKey: "AIzaSyB-az4TQDdG1g8VLfQOOadVj6tYSyKJFRw",
+    authDomain: "olive-3ea8f.firebaseapp.com",
+    projectId: "olive-3ea8f",
+    storageBucket: "olive-3ea8f.appspot.com",
+    messagingSenderId: "871758479451",
+    appId: "1:871758479451:web:965394de5323cd1b64ff7e",
+    measurementId: "G-JZSZBFBMND"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
